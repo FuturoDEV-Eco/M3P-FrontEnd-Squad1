3c9dbd8b8e801c6bb9969ded24790615c6e80d36
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-04-30 00:05:21 -0300

    Instalação de pacotes + estrutura basica page login

---
+++ src/pages/PaginaLogin/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+function PaginaLogin() {
+  return (
+    <Container>
+      <h1>Login Page</h1>
+    </Container>
+  );
+}
+
+export default PaginaLogin;
