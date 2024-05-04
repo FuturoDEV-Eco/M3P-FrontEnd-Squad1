cad00d3d810e3ba0d782eb8c536fa1bb393ba4eb
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-05-03 21:49:46 -0300

    Configuração do theme materialUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
 import routes from "./router/routes.jsx";
 
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#127f18"
+    },
+    secondary: {
+      main: "#ff6d00"
+    }
+  }
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes} />
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={routes} />
+  </ThemeProvider>
 );
