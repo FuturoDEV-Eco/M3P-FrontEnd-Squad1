0f7c71aa90e9cf21f7cb1ccecae05de788535f5c
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-05-06 04:28:05 -0300

    feat: Implementado pagina de lista de pontos coleta

--- src/pages/PaginaListaColeta/index.jsx
+++ src/pages/PaginaListaColeta/index.jsx
@@ -1,7 +1,39 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { PontosColetaContext } from "../../contexts/PontosColeta/PontosColetaContext";
+import { Box, Typography } from "@mui/material";
+import PontoColetaCard from "../../components/PontoColetaCard";
+
 function PaginaListaColeta() {
+  const { pontosColeta, deletarLocalColeta, getPontosColeta } =
+    useContext(PontosColetaContext);
+  const navigate = useNavigate();
+
+  function handleEdit(id) {
+    navigate(`/coleta/cadastro/${id}`);
+  }
+
+  async function handleDelete(id) {
+    await deletarLocalColeta(id);
+    await getPontosColeta();
+  }
   return (
     <>
-      <h1>Lista Pontos de coleta</h1>
+      <Typography variant="h4">Lista Pontos de coleta</Typography>
+      <Box sx={{ display: "grid", gap: 2, maxWidth: 1000, mt: 4, mx: "auto" }}>
+        {pontosColeta.map((pontoColeta) => {
+          return (
+            <PontoColetaCard
+              key={pontoColeta.id}
+              pontoColeta={pontoColeta}
+              zoom={13}
+              scrollWheelZoom={false}
+              onclickEditar={() => handleEdit(pontoColeta.id)}
+              onclickDeletar={() => handleDelete(pontoColeta.id)}
+            />
+          );
+        })}
+      </Box>
     </>
   );
 }
