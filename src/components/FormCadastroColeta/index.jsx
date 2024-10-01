17fbf4bd03dd8098f6cc820f6443ded94bb694d5
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-09-30 22:12:41 -0300

    Commit inicial para ramificação; Cadastro de coletas agora aceita atualização de locais passando id na url; Implementado melhorias no cadastro de coletas, que carecem de revisões

--- src/components/FormCadastroColeta/index.jsx
+++ src/components/FormCadastroColeta/index.jsx
@@ -1,4 +1,13 @@
-import { Box, Button, Grid, MenuItem } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Typography,
+} from "@mui/material";
 import Input from "../Input/Index";
 import { ViaCepService } from "../../services/ViaCepService";
 import { useForm } from "react-hook-form";
@@ -14,7 +23,7 @@ function FormCadastroColeta({ pontoColeta }) {
     handleSubmit,
     setValue,
     getValues,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     defaultValues: {
       nome: "",
@@ -26,24 +35,21 @@ function FormCadastroColeta({ pontoColeta }) {
         numero: "",
         bairro: "",
         cidade: "",
-        estado: ""
+        estado: "",
       },
       latitude: "",
       longitude: "",
-      tipoResiduo: ""
-    }
+      tipoResiduo: [],
+    },
   });
 
   const navigate = useNavigate();
-
   const usuarioLogado = JSON.parse(localStorage.getItem("user"));
-
   const { cadastrarPontoColeta, editarPontoColeta } =
     useContext(PontosColetaContext);
 
   useEffect(() => {
     if (pontoColeta) {
-      console.log(pontoColeta);
       setIsEditMode(true);
       setValue("nome", pontoColeta.nome);
       setValue("descricao", pontoColeta.descricao);
@@ -59,7 +65,28 @@ function FormCadastroColeta({ pontoColeta }) {
     } else {
       setIsEditMode(false);
     }
-  }, [pontoColeta]);
+  }, [pontoColeta, setValue]);
+
+  const buscarCoordenadasPorEndereco = async (endereco) => {
+    const query = `${endereco.logradouro}, ${endereco.numero}, ${endereco.bairro}, ${endereco.cidade}, ${endereco.estado}`;
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+      query
+    )}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.length > 0) {
+        setValue("latitude", data[0].lat);
+        setValue("longitude", data[0].lon);
+      } else {
+        console.error("Endereço não encontrado no Nominatim.");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar coordenadas: ", error);
+    }
+  };
 
   const buscarEnderecoPorCep = async () => {
     let cep = getValues("localizacao.cep");
@@ -70,6 +97,8 @@ function FormCadastroColeta({ pontoColeta }) {
         setValue("localizacao.bairro", response.bairro);
         setValue("localizacao.cidade", response.localidade);
         setValue("localizacao.estado", response.uf);
+
+        await buscarCoordenadasPorEndereco(response);
       }
     } catch (error) {
       console.error("Erro ao buscar o CEP: ", error);
@@ -80,7 +109,6 @@ function FormCadastroColeta({ pontoColeta }) {
     const debounceTimer = setTimeout(async () => {
       await buscarEnderecoPorCep();
     }, 500);
-
     return () => clearTimeout(debounceTimer);
   };
 
@@ -91,7 +119,7 @@ function FormCadastroColeta({ pontoColeta }) {
       numero: getValues("localizacao.numero"),
       bairro: getValues("localizacao.bairro"),
       cidade: getValues("localizacao.cidade"),
-      estado: getValues("localizacao.estado")
+      estado: getValues("localizacao.estado"),
     };
 
     const novoPontoColeta = {
@@ -102,7 +130,7 @@ function FormCadastroColeta({ pontoColeta }) {
       localizacao: localizacao,
       latitude: Number(data.latitude),
       longitude: Number(data.longitude),
-      tipoResiduo: data.tipoResiduo
+      tipoResiduo: data.tipoResiduo,
     };
 
     if (isEditMode) {
@@ -115,116 +143,85 @@ function FormCadastroColeta({ pontoColeta }) {
     }
   };
 
+  const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${getValues(
+    "latitude"
+  )},${getValues("longitude")}`;
+
   return (
     <Box
       component="form"
       sx={{ display: "grid", gap: 1 }}
-      onSubmit={handleSubmit(onSubmit)}>
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <Grid container spacing={2}>
+        {/* Nome do Local */}
         <Grid item xs={12} sm={6}>
-          {!isEditMode ? (
-            <Input
-              id="nome"
-              label="Nome"
-              type="text"
-              register={register("nome", { required: "O nome é obrigatório" })}
-              error={!!errors.nome}
-              helperText={errors.nome?.message}
-            />
-          ) : (
-            <Input
-              id="nome"
-              label="Nome"
-              type="text"
-              register={register("nome", { required: "O nome é obrigatório" })}
-              error={!!errors.nome}
-              helperText={errors.nome?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          {!isEditMode ? (
-            <Input
-              id="tipoResiduo"
-              label="Tipo Resíduo"
-              type="text"
-              register={register("tipoResiduo", {
-                required: "O campo tipoResiduo é obrigatório"
-              })}
-              error={!!errors.tipoResiduo}
-              helperText={errors.tipoResiduo?.message}
-            />
-          ) : (
-            <Input
-              id="tipoResiduo"
-              label="Tipo Resíduo"
-              type="text"
-              register={register("tipoResiduo", {
-                required: "O campo tipoResiduo é obrigatório"
-              })}
-              error={!!errors.tipoResiduo}
-              helperText={errors.tipoResiduo?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          {!isEditMode ? (
-            <Input
-              id="descricao"
-              label="Descrição"
-              multiline
-              type="text"
-              register={register("descricao", {
-                required: "A descrição é obrigatória"
-              })}
-              error={!!errors.descricao}
-              helperText={errors.descricao?.message}
-            />
-          ) : (
-            <Input
-              id="descricao"
-              label="Descrição"
-              multiline
-              type="text"
-              register={register("descricao", {
-                required: "A descrição é obrigatória"
-              })}
-              error={!!errors.descricao}
-              helperText={errors.descricao?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
+          <Input
+            id="nome"
+            label="Nome"
+            type="text"
+            register={register("nome", { required: "O nome é obrigatório" })}
+            error={!!errors.nome}
+            helperText={errors.nome?.message}
+          />
         </Grid>
 
+        {/* Tipo de Resíduo */}
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <InputLabel id="tipoResiduo-label">Tipos de Resíduos</InputLabel>
+            <Select
+              labelId="tipoResiduo-label"
+              id="tipoResiduo"
+              multiple
+              value={getValues("tipoResiduo") || []} // Garante que o valor seja sempre um array
+              onChange={(e) => setValue("tipoResiduo", e.target.value)}
+              renderValue={(selected) => selected.join(", ")}
+            >
+              {[
+                "Vidro",
+                "Metal",
+                "Papel",
+                "Plástico",
+                "Orgânico",
+                "Baterias",
+              ].map((residuo) => (
+                <MenuItem key={residuo} value={residuo}>
+                  {residuo}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        {/* Descrição */}
+        <Grid item xs={12}>
+          <Input
+            id="descricao"
+            label="Descrição"
+            multiline
+            type="text"
+            register={register("descricao", {
+              required: "A descrição é obrigatória",
+            })}
+            error={!!errors.descricao}
+            helperText={errors.descricao?.message}
+          />
+        </Grid>
+
+        {/* CEP e Logradouro */}
         <Grid item xs={12} sm={3}>
-          {!isEditMode ? (
-            <Input
-              id="cep"
-              label="Cep"
-              type="text"
-              register={register("localizacao.cep", {
-                required: "O cep é obrigatório",
-                onBlur: () => handleCepBlur()
-              })}
-              error={!!errors.localizacao?.cep}
-              helperText={errors.localizacao?.cep?.message}
-            />
-          ) : (
-            <Input
-              id="cep"
-              label="Cep"
-              type="text"
-              register={register("localizacao.cep", {
-                required: "O cep é obrigatório",
-                onBlur: () => handleCepBlur()
-              })}
-              error={!!errors.localizacao?.cep}
-              helperText={errors.localizacao?.cep?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
+          <Input
+            id="cep"
+            label="Cep"
+            type="text"
+            register={register("localizacao.cep", {
+              required: "O cep é obrigatório",
+              onBlur: handleCepBlur,
+            })}
+            error={!!errors.localizacao?.cep}
+            helperText={errors.localizacao?.cep?.message}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
           <Input
@@ -232,133 +229,103 @@ function FormCadastroColeta({ pontoColeta }) {
             label="Logradouro"
             type="text"
             register={register("localizacao.logradouro", {
-              required: "O logradouro é obrigatório"
+              required: "O logradouro é obrigatório",
             })}
             error={!!errors.localizacao?.logradouro}
             helperText={errors.localizacao?.logradouro?.message}
-            InputLabelProps={{ shrink: true }}
           />
         </Grid>
         <Grid item xs={12} sm={3}>
-          {!isEditMode ? (
-            <Input
-              id="numero"
-              label="Número"
-              type="text"
-              register={register("localizacao.numero", {
-                required: "O número é obrigatório"
-              })}
-              error={!!errors.localizacao?.numero}
-              helperText={errors.localizacao?.numero?.message}
-            />
-          ) : (
-            <Input
-              id="numero"
-              label="Número"
-              type="text"
-              register={register("localizacao.numero", {
-                required: "O número é obrigatório"
-              })}
-              error={!!errors.localizacao?.numero}
-              helperText={errors.localizacao?.numero?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
+          <Input
+            id="numero"
+            label="Número"
+            type="text"
+            register={register("localizacao.numero", {
+              required: "O número é obrigatório",
+            })}
+            error={!!errors.localizacao?.numero}
+            helperText={errors.localizacao?.numero?.message}
+          />
         </Grid>
-        <Grid item xs={12} sm={6}>
+
+        {/* Bairro, Cidade e Estado */}
+        <Grid item xs={12} sm={4}>
           <Input
             id="bairro"
             label="Bairro"
             type="text"
             register={register("localizacao.bairro", {
-              required: "O bairro é obrigatório"
+              required: "O bairro é obrigatório",
             })}
             error={!!errors.localizacao?.bairro}
             helperText={errors.localizacao?.bairro?.message}
-            InputLabelProps={{ shrink: true }}
           />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={4}>
           <Input
             id="cidade"
             label="Cidade"
             type="text"
             register={register("localizacao.cidade", {
-              required: "A cidade é obrigatória"
+              required: "A cidade é obrigatória",
             })}
             error={!!errors.localizacao?.cidade}
             helperText={errors.localizacao?.cidade?.message}
-            InputLabelProps={{ shrink: true }}
           />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={4}>
           <Input
             id="estado"
             label="Estado"
             type="text"
             register={register("localizacao.estado", {
-              required: "O estado é obrigatório"
+              required: "O estado é obrigatório",
             })}
             error={!!errors.localizacao?.estado}
             helperText={errors.localizacao?.estado?.message}
-            InputLabelProps={{ shrink: true }}
+          />
+        </Grid>
+
+        {/* Latitude e Longitude */}
+        <Grid item xs={12} sm={6}>
+          <Input
+            id="latitude"
+            label="Latitude"
+            type="text"
+            register={register("latitude", {
+              required: "A latitude é obrigatória",
+            })}
+            error={!!errors.latitude}
+            helperText={errors.latitude?.message}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          {!isEditMode ? (
-            <Input
-              id="latitude"
-              label="Latitude"
-              type="text"
-              register={register("latitude", {
-                required: "A latitude é obrigatória"
-              })}
-              error={!!errors.latitude}
-              helperText={errors.latitude?.message}
-            />
-          ) : (
-            <Input
-              id="latitude"
-              label="Latitude"
-              type="text"
-              register={register("latitude", {
-                required: "A latitude é obrigatória"
-              })}
-              error={!!errors.latitude}
-              helperText={errors.latitude?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
+          <Input
+            id="longitude"
+            label="Longitude"
+            type="text"
+            register={register("longitude", {
+              required: "A longitude é obrigatória",
+            })}
+            error={!!errors.longitude}
+            helperText={errors.longitude?.message}
+          />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          {!isEditMode ? (
-            <Input
-              id="longitude"
-              label="Longitude"
-              type="text"
-              register={register("longitude", {
-                required: "A longitude é obrigatória"
-              })}
-              error={!!errors.longitude}
-              helperText={errors.longitude?.message}
-            />
-          ) : (
-            <Input
-              id="longitude"
-              label="Longitude"
-              type="text"
-              register={register("longitude", {
-                required: "A longitude é obrigatória"
-              })}
-              error={!!errors.longitude}
-              helperText={errors.longitude?.message}
-              InputLabelProps={{ shrink: true }}
-            />
-          )}
+
+        {/* Link para o Google Maps */}
+        <Grid item xs={12}>
+          <Typography variant="body1" sx={{ mt: 1 }}>
+            Link para o Google Maps:{" "}
+            <a href={googleMapsLink} target="_blank" rel="noopener noreferrer">
+              {googleMapsLink}
+            </a>
+          </Typography>
         </Grid>
-        <Grid item xs={12} sm={4} sx={{ mb: 2, mx: "auto" }}>
-          <Button variant="contained" type="submit" fullWidth>
-            Cadastrar
+
+        {/* Botões de Ação */}
+        <Grid item xs={12}>
+          <Button variant="contained" type="submit">
+            {isEditMode ? "Atualizar" : "Cadastrar"} Ponto de Coleta
           </Button>
         </Grid>
       </Grid>
