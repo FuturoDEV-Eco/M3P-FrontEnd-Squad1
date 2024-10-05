3d169b624131c5f167a7f9302cacb612ce0c885a
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-10-05 01:29:57 -0300

    Ajuste Dashboard após integração com backend

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -6,6 +6,7 @@ import MapView from "../../components/MapView";
 import PeopleIcon from "@mui/icons-material/People";
 import LocationOnIcon from "@mui/icons-material/LocationOn"; // Ícone de localização
 import NavMenu from "../../components/NavMenu";
+import { Api } from "../../services/api";
 
 // Defina os links de navegação
 const navArrayLinks = [
@@ -14,31 +15,45 @@ const navArrayLinks = [
   { title: "Login", path: "/login" }
 ];
 
-
 const Dashboard = () => {
   const [pontosColeta, setPontosColeta] = useState([]);
-  const [usuariosCadastrados, setUsuariosCadastrados] = useState(0); // Quantidade de usuários cadastrados
+  const [totalPontosColeta, setTotalPontosColeta] = useState(0);
+  const [totalUsuariosCadastrados, setTotalUsuariosCadastrados] = useState(0); // Quantidade de usuários cadastrados
 
   useEffect(() => {
-    fetchPontosColeta();
-    fetchUsuariosCadastrados(); // Chama a função para buscar usuários cadastrados
+    fetchDashboard();
+    fetchTotalPontosColeta();
+    fetchTotalUsuariosCadastrados(); // Chama a função para buscar usuários cadastrados
   }, []);
 
-  const fetchPontosColeta = async () => {
+  const fetchDashboard = async () => {
     try {
-      const response = await fetch("http://localhost:3000/locaisColeta");
+      const response = await Api.get("/dashboard");
+      setPontosColeta(response.data);
+    } catch (error) {
+      console.error("Erro ao listar os locais da dashboard:", error);
+    }
+  };
+
+  const fetchTotalPontosColeta = async () => {
+    try {
+      const response = await Api.get("/dashboard/totalLocais");
+      setTotalPontosColeta(response.data);
+      /* const response = await fetch("http://localhost:3000/locaisColeta");
       const data = await response.json();
-      setPontosColeta(data);
+      setPontosColeta(data); */
     } catch (error) {
       console.error("Erro ao carregar os pontos de coleta:", error);
     }
   };
 
-  const fetchUsuariosCadastrados = async () => {
+  const fetchTotalUsuariosCadastrados = async () => {
     try {
-      const response = await fetch("http://localhost:3000/usuarios");
+      const reponse = await Api.get("/dashboard/totalUsuarios");
+      setTotalUsuariosCadastrados(reponse.data);
+      /* const response = await fetch("http://localhost:3000/usuarios");
       const data = await response.json();
-      setUsuariosCadastrados(data.length); // Exibe a quantidade de usuários
+      setUsuariosCadastrados(data.length); // Exibe a quantidade de usuários */
     } catch (error) {
       console.error("Erro ao carregar os usuários cadastrados:", error);
     }
@@ -46,31 +61,31 @@ const Dashboard = () => {
 
   return (
     <>
-    <NavMenu navArrayLinks={navArrayLinks} /> 
-    <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={3}>
-          <CardInfo
-            title="Usuários Cadastrados"
-            value={usuariosCadastrados}
-            icon={<PeopleIcon />}
-          />
+      <NavMenu navArrayLinks={navArrayLinks} />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={3}>
+            <CardInfo
+              title="Usuários Cadastrados"
+              value={totalUsuariosCadastrados}
+              icon={<PeopleIcon />}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <CardInfo
+              title="Pontos de Coleta"
+              value={totalPontosColeta}
+              icon={<LocationOnIcon />} // Novo ícone de localização
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <MapView pontosColeta={pontosColeta} />
+          </Grid>
+          <Grid item xs={12}>
+            <ColetaList pontosColeta={pontosColeta} />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <CardInfo
-            title="Pontos de Coleta"
-            value={pontosColeta.length}
-            icon={<LocationOnIcon />} // Novo ícone de localização
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <MapView pontosColeta={pontosColeta} />
-        </Grid>
-        <Grid item xs={12}>
-          <ColetaList pontosColeta={pontosColeta} />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
     </>
   );
 };
